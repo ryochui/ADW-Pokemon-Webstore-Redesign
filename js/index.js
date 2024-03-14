9445697bd2ae2c6a3a2c6fbd506871176b1c40d7
odysseum_ <202210087@fit.edu.ph>
2024-03-15 00:59:32 +0800

    15/03/2023 - Cleaned up CSS, Started on News, Making UI

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
-document.querySelector("#hamburger-menu").addEventListener("click", toggleSideBar);
 let nav = document.querySelector("nav");
+let sections = document.querySelectorAll("section");
 let hamburger = document.querySelector("#hamburger-menu");
 
-function toggleSideBar() {
+hamburger.addEventListener("click", function toggleSideBar() {
     nav.classList.toggle("active");
     hamburger.classList.toggle("active");
-}
+});
+
+sections.forEach(section => {
+    section.addEventListener("click", function toggleSideBar() {
+        if (nav.classList.contains("active")) {
+            nav.classList.toggle("active");
+            hamburger.classList.toggle("active");
+        }
+    });
+});
+
