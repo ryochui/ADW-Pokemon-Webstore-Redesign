249859e9b41d9702fb5a89bdc0d2b66cc93043a5
ryochui <baronia.nadinekiandra@gmail.com>
2024-03-20 20:24:39 +0800

    20/03/24 - Payment DONE

---
+++ js/payment.js
@@ -0,0 +1,5 @@
+const payButton = document.getElementById('payButton');
+
+payButton.addEventListener('click', function() {
+    alert("You will be redirected to the corresponding payment center.");
+});
